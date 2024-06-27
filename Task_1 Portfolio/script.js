889480f9294f7b0a0f44039e232a69b44e7e903f
ashish-8690 <ashishnkt8690@gmail.com>
2024-06-27 18:44:16 +0530

    Add files via upload

---
+++ Task_1 Portfolio/script.js
@@ -0,0 +1,81 @@
+// Show menu
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+if(navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu')
+    });
+}
+
+if(navClose){
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu')
+    });
+}
+
+// Remove menu mobile
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = ()=>{
+    navMenu.classList.remove('show-menu');
+};
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// Add blur header
+const blurHeader = () => {
+    const header = document.getElementById('header');
+    this.scrollY >= 50 ? header.classList.add("blur-header")
+                       : header.classList.remove("blur-header") 
+}
+window.addEventListener("scroll", blurHeader);
+
+
+// EMAIl JS
+const contactForm = document.getElementById("contact-form"),
+contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) =>{
+    e.preventDefault()
+    emailjs.sendForm('')
+    .then(()=>{
+        contactMessage.textContent = "Message sent successfully"
+        setTimeout(()=>{
+            contactMessage.textContent = ''
+        }, 5000)
+    })
+    contactForm.reset()
+}
+
+contactForm.addEventListener('submit', sendEmail)
+
+// Breakpoints
+const widthMatch = window.matchMedia("(max-width: 1300px)");
+var w = window.innerWidth;
+if (w<=1300){
+    rect(true);
+}
+function rect(x){
+    if (x) {
+        document.getElementById("rect-one").classList.remove("rect_one");
+        // document.getElementById("rect-two").classList.remove("rect_two");
+    }
+    else{
+        document.getElementById("rect-one").classList.add("rect_one");
+        // document.getElementById("rect-two").classList.add("rect_two");
+    }
+}
+widthMatch.addEventListener("change", (mm) => {
+    rect(mm.matches)
+});
+
+// Download CV
+function downloadCV() {
+    const link = document.createElement('a');
+    link.href = './assets/cv.pdf';
+    link.download = 'Ashish_CV.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
